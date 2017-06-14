73593591a54048f282c9129adf0102451e9fe574
Jintae Joo <jtjoo@me.com>
2017-06-14 21:19:24 +0900

    수정사항 적용

--- js/init.js
+++ js/init.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
 	$('[data-toggle="tooltip"]').tooltip();
+	resetBodyHeight();
 });
+
+$(window).on('resize', function(event) {
+	// event.preventDefault();
+	resetBodyHeight();
+});
+
+function resetBodyHeight() {
+	var wh = $(window).height();
+	if ( wh > 840 ) {
+		$("body").css("height","100%");
+	} else {
+		$("body").css("height","auto");
+	}
+}
